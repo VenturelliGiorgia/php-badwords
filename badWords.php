dea54ead92f64ead6c4e2ddf6c70ce381e571d0e
VenturelliGiorgia <venturelli.giorgia.03@gmail.com>
2022-12-19 19:04:47 +0100

    lunghezza paragrafo

--- badWords.php
+++ badWords.php
@@ -4,7 +4,10 @@ $paragrafo = $_POST["paragrafo"];
 $censura = $_POST["censura"];
 
 echo 'Paragrafo originale: ' . $paragrafo . '<br>';
-echo 'Parola da censusare: ' . $censura . '<br><br>';
-echo 'Frase censurata: ' . str_ireplace($censura, 'xxx', $paragrafo);
+echo 'Parola da censusare: ' . $censura . '<br>';
+echo 'lunghezza frase:' . strlen($paragrafo) . "<br><br>";
+$badWord = str_ireplace($censura, 'xxx', $paragrafo);
+echo 'Frase censurata: ' . $badWord . '<br>';
+echo 'lunghezza frase censusata: ' .strlen($badWord);
 
 ?>
